aea7e858f56821301c79d257dd9343c951b4cf4c
Saramunoz2023 <sara@iipsa.com.ar>
2023-05-30 22:50:43 -0300

    Revert "Initial commit"
    This reverts commit 45c0ebaecf6291686be352fdff05a8f61b405de6.

--- script.js
+++
@@ -1,8 +0,0 @@
-function showAlert() { 
-    alert("¡Hola, esta es una alerta desde JavaScript!"); 
-}
-
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-           document.querySelector(".nav-links").
-                      classList.toggle("nav-links-responsive")})
